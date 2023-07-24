c7a9a971182b0f44c1c2ae13b65ca21078e05a6a
abukawsar45 <kawsar1146236@gmail.com>
2023-07-24 11:04:52 +0600

    void function

--- built-in-type/index.js
+++ built-in-type/index.js
@@ -3,8 +3,23 @@ var lastName;
 var fullName;
 var age;
 var userId;
+var isStudent;
 firstName = 'Abu';
 lastName = 'KawSar';
 fullName = firstName.concat(lastName);
 age = 22;
 console.log(fullName, age);
+// const checkFisVoid = (a,b) : void => {
+//   console.log('oky, i understand');
+//   return a+b
+// }
+// const sum = checkFisVoid(3, 6)
+// console.log(sum)
+function checkFisVoid() {
+    console.log('Okay, I understand');
+    // return 33;
+}
+;
+// checkFisVoid()
+var sum = checkFisVoid();
+console.log(sum);
--- built-in-type/index.ts
+++ built-in-type/index.ts
@@ -3,9 +3,25 @@ let lastName: string;
 let fullName: string;
 let age: number;
 let userId: number;
+let isStudent: boolean;
 
 firstName = 'Abu';
 lastName = 'KawSar';
 fullName = firstName.concat(lastName);
 age = 22;
-console.log(fullName,age)
+console.log(fullName, age);
+
+// const checkFisVoid = (a,b) : void => {
+//   console.log('oky, i understand');
+//   return a+b
+// }
+// const sum = checkFisVoid(3, 6)
+// console.log(sum)
+function checkFisVoid  ()   {
+  console.log('Okay, I understand');
+  // return 33;
+};
+
+// checkFisVoid()
+const sum = checkFisVoid();
+console.log(sum);
