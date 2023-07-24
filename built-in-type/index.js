20fc6f34981a0d185139bff18da5dd4185a9672c
abukawsar45 <kawsar1146236@gmail.com>
2023-07-24 10:49:16 +0600

    built-in-type method apply

---
+++ built-in-type/index.js
@@ -0,0 +1,10 @@
+var firstName;
+var lastName;
+var fullName;
+var age;
+var userId;
+firstName = 'Abu';
+lastName = 'KawSar';
+fullName = firstName.concat(lastName);
+age = 22;
+console.log(fullName, age);
---
+++ built-in-type/index.ts
@@ -0,0 +1,11 @@
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let age: number;
+let userId: number;
+
+firstName = 'Abu';
+lastName = 'KawSar';
+fullName = firstName.concat(lastName);
+age = 22;
+console.log(fullName,age)
